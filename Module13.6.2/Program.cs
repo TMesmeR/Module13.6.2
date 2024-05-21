72635dcec449d873fc88f9d30c66d7d5f4b72f8f
TMesmeR <mesmerat@gmail.com>
2024-05-21 13:18:46 +0300

    Добавьте файлы проекта.

---
+++ Module13.6.2/Program.cs
@@ -0,0 +1,6 @@
+﻿using Module13._6._2;
+
+string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Text1.txt");
+
+var analyzer = new WordFrequencyAnalyzer(filePath);
+analyzer.Analyze();
